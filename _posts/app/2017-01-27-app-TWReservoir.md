18b91a7ee82e94745e5ec4f97557ccb9688101c2
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-02-27 15:21:34 +0800

    modify post

---
+++ _posts/app/2017-01-27-app-TWReservoir.md
@@ -0,0 +1,23 @@
+---
+title:  "台灣即時水情APP"
+subtitle: ""
+author: "APP"
+avatar: "img/icon/app.png"
+image: "img/app/app-01.png"
+date:   2017-01-27
+---
+
+Android、Http Request、SQLite
+
+## 簡介
+ 
+	2015年台灣旱災缺水危機發生於2015年1至6月，缺水危機近因為2014年台灣各地秋冬以來降雨不佳，為自1947年以來最嚴重旱災，所以我藉由學校行動裝置程式設計所學來開發一個能夠察看各地水庫即時水量的應用程式。
+
+## 功能介紹
+此App是利用經濟水利署水利資料平台的即時水庫水量資訊，能夠查詢全台二十個水庫及資訊，內部欄位包含有效蓄水量、今日進水量、以及預測剩餘天數。
+
+![](img/app/app-01-1.png)
+
+GitHub: [Link](https://github.com/andy6804tw/TWReservoir)
+
+Google Play商店下載：[Link](https://play.google.com/store/apps/details?id=com.f74372017.twreservoir&hl=zh_TW)
--- js/modern-blog.js
+++ js/modern-blog.js
@@ -263,3 +263,56 @@ function closeCard(id){
       , 1700)
   }
 }
+
+
+
+var _mapPolygons = function (pattern) {
+
+  // Append SVG to pattern container.
+  $(SELECTORS.pattern).append(pattern);
+
+  // Convert nodelist to array,
+  // Used `.childNodes` because IE doesn't support `.children` on SVG.
+  polygonMap.paths = [].slice.call(pattern.childNodes);
+
+  polygonMap.points = [];
+
+  polygonMap.paths.forEach(function (polygon) {
+
+    // Hide polygons by adding CSS classes to each svg path (used attrs because of IE).
+    $(polygon).attr('class', CLASSES.polygon);
+
+    var rect = polygon.getBoundingClientRect();
+
+    var point = {
+      x: rect.left + rect.width / 2,
+      y: rect.top + rect.height / 2
+    };
+
+    polygonMap.points.push(point);
+  });
+
+  // All polygons are hidden now, display the pattern container.
+  $(SELECTORS.pattern).removeClass(CLASSES.patternHidden);
+};
+/**
+     * Enum of CSS selectors.
+     */
+var SELECTORS = {
+  pattern: '.pattern',
+  card: '.card',
+  cardImage: '.card__image',
+  cardClose: '.card__btn-close'
+};
+// For options see: https://github.com/qrohlf/Trianglify
+var pattern = Trianglify({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  cell_size: 90,
+  variance: 1,
+  stroke_width: 1,
+  x_colors: 'random',
+  y_colors: 'random'
+}).svg(); // Render as SVG.
+
+_mapPolygons(pattern);
